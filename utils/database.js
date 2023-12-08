9d6d500aef43acb23bbdab77802e6698e1515afa
FinoanaRandria <110087713+FinoanaRandria@users.noreply.github.com>
2023-12-08 07:54:01 +0300

    backup

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+let  isConnected = false
+//conection Mongodb
+export const connectToDB = async()=>{
+    mongoose.set('strictQuery', true)
+
+    if(isConnected){
+         console.log('Mongodb is already connceted');
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,{
+            dbName:'share_prompt',
+            useNewUrlParser:true,
+            useUnifedTopology:true,
+        })
+        isConnected = true
+        console.log('MongoDb connected');
+    } catch (error) {
+        console.log(error);
+    }
+}
